1c4f7827f136544b57da18989df0ead9dacf0e0d
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2017-07-19 14:09:30 -0500

    Got oo writers to work

--- irida-sistr-results.py
+++ irida-sistr-results.py
@@ -1,5 +1,6 @@
 import argparse, sys
 import csv, xlsxwriter
+import abc
 import requests
 import json
 import logging
@@ -201,6 +202,81 @@ class IridaSistrResults:
 
 		return sistr_results
 
+class SistrResultsWriter(object):
+
+	def __init__(self,irida_url):
+		__metaclass__ = abc.ABCMeta
+		self.irida_url=irida_url
+
+	@abc.abstractmethod
+	def _write_row(self,row):
+		return
+
+	@abc.abstractmethod
+	def _write_header(self,header):
+		return
+
+	def write(self,sistr_results):
+		
+		header = [
+			'Sample Name',
+			'Serovar',
+			'Serovar Antigen',
+			'Serovar cgMLST',
+			'QC Status',
+			'QC Messages',
+			'URL',
+			'IRIDA Sample Identifer',
+			'IRIDA File Pair Identifier'
+		]
+
+		self._write_header(header)
+		
+		for result in sistr_results.values():
+			sample = result['sample']
+	
+			if (not result['has_results']):
+				self._write_row([sample['sampleName']])
+			else:
+				paired=result['paired_files'][0]
+				sistr_predictions = result['sistr_predictions'][0]
+		
+				submission_identifier=result['submission']['identifier']
+				submission_url=self.irida_url
+				if submission_url.endswith('/'):
+					submission_url += 'analysis/'
+				else:
+					submission_url += '/analysis/'
+				submission_url += submission_identifier
+		
+				if (sistr_predictions['serovar_cgmlst'] is None):
+					sistr_predictions['serovar_cgmlst']='None'
+		
+				self._write_row([
+					sample['sampleName'],
+					sistr_predictions['serovar'],
+					sistr_predictions['serovar_antigen'],
+					sistr_predictions['serovar_cgmlst'],
+					sistr_predictions['qc_status'],
+					sistr_predictions['qc_messages'],
+					submission_url,
+					sample['identifier'],
+					paired['identifier']
+				])
+
+class SistrCsvWriter(SistrResultsWriter):
+
+	def __init__(self, irida_url, out_file):
+		super(SistrCsvWriter,self).__init__(irida_url)
+		self.writer = csv.writer(out_file, delimiter = "\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+	def _write_header(self,header):
+		self.writer.writerow(header)
+
+	def _write_row(self,row):
+		self.writer.writerow(row)
+		
+
 def sistr_results_to_excel(sistr_results, irida_url, excel_file):
 	workbook = xlsxwriter.Workbook(excel_file)
 	worksheet = workbook.add_worksheet()
@@ -296,47 +372,6 @@ def sistr_results_to_excel(sistr_results, irida_url, excel_file):
 
 	workbook.close()
 
-def sistr_results_to_table(sistr_results, table_file, irida_url):
-	sistr_writer = csv.writer(table_file, delimiter = "\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-	sistr_writer.writerow([
-		'SampleName',
-		'Serovar',
-		'SerovarAntigen',
-		'SerovarCgMLST',
-		'QCStatus',
-		'URL'
-	])
-
-	for result in sistr_results.values():
-		sample = result['sample']
-
-		if (not result['has_results']):
-			sistr_writer.writerow([sample['sampleName']])
-		else:
-			paired=result['paired_files'][0]
-			sistr_predictions = result['sistr_predictions'][0]
-	
-			submission_identifier=result['submission']['identifier']
-			submission_url=irida_url
-			if irida_url.endswith('/'):
-				submission_url += 'analysis/'
-			else:
-				submission_url += '/analysis/'
-			submission_url += submission_identifier
-	
-			if (sistr_predictions['serovar_cgmlst'] is None):
-				sistr_predictions['serovar_cgmlst']='None'
-	
-			sistr_writer.writerow([
-				sample['sampleName'],
-				sistr_predictions['serovar'],
-				sistr_predictions['serovar_antigen'],
-				sistr_predictions['serovar_cgmlst'],
-				sistr_predictions['qc_status'],
-				submission_url
-			])
-
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Compile SISTR results from an IRIDA instance into a table.')
 
@@ -376,7 +411,8 @@ if __name__ == '__main__':
 	sistr_list=irida_results.get_sistr_results(1)
 	
 	if arg_dict['tabular']:
-		sistr_results_to_table(sistr_list,sys.stdout, arg_dict['irida_url'])
+		writer=SistrCsvWriter(arg_dict['irida_url'],sys.stdout)
+		writer.write(sistr_list)
 
 	if arg_dict['excel_file'] is not None:
 		sistr_results_to_excel(sistr_list,arg_dict['irida_url'], arg_dict['excel_file'])
