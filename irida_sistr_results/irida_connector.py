22abd58e2c8cc60820f82b4373a70870da3273be
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2017-07-20 16:20:48 -0500

    Fixed up url parsing for complicated urls

--- irida_sistr_results/irida_connector.py
+++ irida_sistr_results/irida_connector.py
@@ -2,10 +2,14 @@ import json
 import logging
 import ast
 from rauth import OAuth2Service
+from urlparse import urlsplit, urljoin
 
 class IridaConnector(object):
 
 	def __init__(self,client_id,client_secret,username,password,base_url):
+		base_url=base_url.rstrip('/')
+		self._base_path=urlsplit(base_url).path
+
 		access_token_url=base_url+'/api/oauth/token'
 	
 		params = {
@@ -30,8 +34,8 @@ class IridaConnector(object):
 		self.session=oauth_service.get_session(token)
 
 	def get(self,path):
-		response=self.session.get(path)
 		logging.debug("Getting path="+path)
+		response=self.session.get(path)
 
 		if (response.ok):
 			self._log_json(response.json())
@@ -39,10 +43,24 @@ class IridaConnector(object):
 		else:
 			response.raise_for_status()
 
+	def _join_path(self,path):
+		if (self._base_path is None or self._base_path == ''):
+			return path
+		# If passed full URL like http://localhost/path, don't add on base_path
+		elif (urlsplit(path).scheme != ''):
+			return path
+		else:
+			if (path[0] == '/'):
+				return self._base_path + path
+			else:
+				return self._base_path + '/' + path
+
 	def get_resources(self,path):
+		path=self._join_path(path)
 		return self.get(path)['resources']
 
 	def get_file(self, path):
+		path=self._join_path(path)
 		logging.debug("Getting file from path="+path)
 		return self.session.get(path, headers={'Accept': 'text/plain'})
 
