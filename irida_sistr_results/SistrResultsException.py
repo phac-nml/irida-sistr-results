97458784abf8fe2d333c8b9db69b8e036a715137
Aaron Petkau <aaron.petkau@canada.ca>
2018-08-02 10:45:23 -0500

    Reformat all files

--- irida_sistr_results/SistrResultsException.py
+++ irida_sistr_results/SistrResultsException.py
@@ -2,7 +2,8 @@
 An Exception raised if there was an issue parsing SISTR results.
 """
 
+
 class SistrResultsException(Exception):
 
-	def __init__(self, msg):
-		super().__init__(msg)
+    def __init__(self, msg):
+        super().__init__(msg)
